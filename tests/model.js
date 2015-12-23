bca8e4bd31ad4fff46a84ff6abd758cb6b70b501
Dmitry Andriyanov <dima117a@yandex-team.ru>
2015-12-23 22:07:41 +0300

    - sinon.js - названия тестов для модели - тест на массовое присвоение значений

--- tests/model.js
+++ tests/model.js
@@ -8,16 +8,38 @@ describe('model', function() {
         expect(obj.get('name')).to.equal('testname');
     });
 
-    it('при изменении данных должно генерироваться событие change', function() {
-        var obj = new lib.model(),
-            called = false;
+    describe('можно передавать', function() {
+        it('хэш с массовым присвоением полей в сеттер', function() {
+            var obj = new lib.model();
 
-        obj.on('change', function() {
-            called = true;
+            obj.set({ a: 1, b: 2 });
+
+            expect(obj.get('a')).to.equal(1);
+            expect(obj.get('b')).to.equal(2);
         });
+        it.skip('хэш с массовым присвоением полей в конструктор');
+    });
+
+    it('при изменении любого поля генерируется change', function() {
+        var callback = sinon.spy(),
+            obj = new lib.model();
+
+        obj.on('change', callback);
 
         obj.set('name', 123);
 
-        expect(called).to.be.true;
+        expect(callback.called).to.be.true;
+    });
+
+    it.skip('при изменении каждого поля генерируется change:field');
+    it.skip('в change:field передается новое значение');
+    it.skip('при массовом изменении change генерируется только один раз');
+    it.skip('если новое значение объект, то набор ключей и их значений сохранятся');
+
+    describe('если старое значение - объект', function() {
+        it.skip('ключи, отсутствующие в новом значении, удалятся (с генерацией событий)');
+        it.skip('новые ключи добавятся (с генерацией событий)');
+        it.skip('значения в существующих ключах изменятся (с генерацией событий)');
+        it.skip('одинаковые значения в существующих ключах не будут генерировать события');
     });
 });
